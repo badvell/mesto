a7d2caeb54c4f2f2b1dd7d6c69532bee3615a4ee
badvell <badvell@mail.ru>
2021-11-03 19:41:31 +0100

    for Review

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ document.querySelector(".profile__button-edit").addEventListener("click", functi
   document.querySelector(".popup").classList.add("active");
 });
 
-document.querySelector(".popup__icon_close").addEventListener("click", function () {
+document.querySelector(".popup__icon-close").addEventListener("click", function () {
   document.querySelector(".popup").classList.remove("active");
 });
 
 function openPopup() {
-  document.querySelector(".popup__title").value = document.querySelector(".profile__author_title").innerText;
-  document.querySelector(".popup__description").value = document.querySelector(".profile__author_description").innerText;
+  document.querySelector(".popup__title").value = document.querySelector(".profile__author-title").innerText;
+  document.querySelector(".popup__description").value = document.querySelector(".profile__author-description").innerText;
   document.querySelector(".popup").classList.add('popup_opened');
 };
 
@@ -20,8 +20,8 @@ function formSubmitHandler(evt) {
   var nameValue = document.querySelector(".popup__title").value;
   var jobValue = document.querySelector(".popup__description").value;
 
-  document.querySelector(".profile__author_title").textContent = nameValue;
-  document.querySelector(".profile__author_description").textContent = jobValue;
+  document.querySelector(".profile__author-title").textContent = nameValue;
+  document.querySelector(".profile__author-description").textContent = jobValue;
 }
 
 document.querySelector(".popup__button").addEventListener("click", function () {
